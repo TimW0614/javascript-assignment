fab2ce8b69debb97247a495fc77813e28dafd9d6
Tim Williams <tikytimbo5@yahoo.com>
2017-08-07 05:55:50 -0600

    added filler text

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<title>javascript-challenge</title>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!-- Custom CSS Here -->
+		<link href="css/style.css" rel="stylesheet"/>
+		<!-- HTML shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script>src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"</script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<title>Bootstrap Challenge</title>
+
+	</head>
+	<body>
+		<h4>javascript-challenge</h4>
+
+		<!-- Photo and text section -->
+		<section class="bg-info text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+					</div>
+					<div class="col-md-7 col-md-offset-1"/>
+					<br/>
+					<br/>
+					<p>
+						Among going manor who did. Do ye is celebrated it sympathize considered. May ecstatic did surprise
+						elegance the ignorant age. Own her miss cold last. It so numerous if he outlived disp</p><br/>
+					<p>Wrote water woman of heart it total other. By in entirely securing suitable graceful at families
+						improved. Zealously few furniture repulsive was agreeable consisted difficult.
+						Collected breakfast estimable questions in to favourite it.</p>
+				</div>
+			</div>
+		</section>
+
+			<div class="col-md-6 col-sm-6">
+				<h2>javascript-challenge</h2>
+				<p>The Warriors finished the season at 67-15, won the Pacific Dision Championship and entered the playoffs
+					as the #1 seed.</p>
+			</div><!-- /.col-md-3 col-sm-6 -->
+
+
+
+	</body>
+
+
+</html>
+
+
